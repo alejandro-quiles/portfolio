663c3efe3d0726ac8ca1340e7b0a425018775ea4
aquiles <alex.quiles13@gmail.com>
2023-12-07 16:40:55 +0100

    welcome hecho

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+import Welcome from '../Welcome/Welcome'
+
+const Home = () => {
+  return (
+    <>
+        <Welcome></Welcome>
+    </>
+  )
+}
+
+export default Home
---
+++ src/Components/Welcome/Welcome.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Welcome.css';
+
+const Welcome = () => {
+    return (
+        <>
+            <div className='welcome-container'>
+                <div className='titulo-container'>
+                    <h4>Hello!</h4>
+                    <div className='linea-home'></div>
+                </div>
+                <section id="section07" className="demo">
+                    <a href='section#00'><span></span><span></span><span></span></a>
+                </section>
+            </div>
+        </>
+    )
+}
+
+export default Welcome
